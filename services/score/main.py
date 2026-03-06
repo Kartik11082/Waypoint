05dc3f51ccebde227314e0ae59eb5c301b6e7dd7
Kartik11082 <karkerakartik27@gmail.com>
2026-03-05 20:28:05 -0600

    Initialized project using uv

---
+++ services/score/main.py
@@ -0,0 +1,6 @@
+# A fastapi post requst endpoint
+# Accepts: lat, long, correct_lat, correct_long, clused_used, seconds_taken
+# returns: score, distance_in_km, verdict
+# Haversine inline, scoring formula inline
+# no database not external calls
+
